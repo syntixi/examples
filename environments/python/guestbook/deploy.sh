ec550bb9b316bb1be32802770a245d9ca98853de
Ta-Ching Chen <tachingchen@srcmesh.com>
2020-10-30 01:16:00 +0800

    fix: Remove env flag when creating bundle

--- environments/python/guestbook/deploy.sh
+++ environments/python/guestbook/deploy.sh
@@ -14,8 +14,8 @@ fi
 syntixi env get --name python || syntixi env create --name python --image syntixi/python-env
 
 # Register functions and routes with syntixi
-syntixi bundle create --name guestbook-get-pkg --env python --code get.py
+syntixi bundle create --name guestbook-get-pkg --code get.py
 syntixi function create --name guestbook-get --env python --bundle guestbook-get-pkg --url /guestbook --method GET
 
-syntixi bundle create --name guestbook-add-pkg --env python --code add.py
+syntixi bundle create --name guestbook-add-pkg --code add.py
 syntixi function create --name guestbook-add --env python --bundle guestbook-add-pkg --url /guestbook --method POST
